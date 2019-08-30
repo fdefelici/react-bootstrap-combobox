3756667b9cde47774b7c8042953f8dcd2c13903d
sickk <s.millevolte@gmail.com>
2019-08-30 22:51:14 +0200

    first commit

---
+++ src/Dropdown.js
@@ -0,0 +1,173 @@
+import React, { Component } from "react";
+
+import DropdownCss from "./Dropdown.css";
+
+class Dropdown extends Component {
+
+  data = []
+  placeholderDefault = ""
+  labelElement = ""
+  maxElementPlaceholder = 0
+  labels = {
+    action: "Select",
+    select: "selected",
+    singular: "item",
+    plural: "items",
+    selectAll: "Select All",
+    deselectAll: "Deselect All"
+  }
+  isMultiSelect = false
+  showButtonsSelectAll = false
+
+  
+  constructor(props) {
+    super(props)
+    
+    if(this.props.labels) {
+      if(this.props.labels.action) this.labels.action = this.props.labels.action
+      if(this.props.labels.select) this.labels.select = this.props.labels.select
+      if(this.props.labels.singular) this.labels.singular = this.props.labels.singular
+      if(this.props.labels.plural) this.labels.plural = this.props.labels.plural
+      if(this.props.labels.selectAll) this.labels.selectAll = this.props.labels.selectAll
+      if(this.props.labels.deselectAll) this.labels.deselectAll = this.props.labels.deselectAll
+    }
+
+    this.data = this.props.data? [...this.props.data].map((each,index) => { return {label: each, index: index} }): []
+    this.placeholderDefault = `${this.labels.action} ${this.labels.singular}`
+    this.maxElementPlaceholder = this.props.maxElementPlaceholder? this.props.maxElementPlaceholder: 0
+    this.isMultiSelect = this.props.isMultiSelect? this.props.isMultiSelect: false
+    this.showButtonsSelectAll = this.props.showButtonsSelectAll? this.props.showButtonsSelectAll: false
+
+    this.state = {
+      placeholder: "",
+      isOpen: false,
+      dataFiltered : [...this.data],
+      selected: []
+    }
+
+  }
+
+  closeOrOpen = () => {
+    this.setState({isOpen:!this.state.isOpen})
+  };
+
+  selectElement = (element) => {
+    
+    if(this.isMultiSelect){
+      let newSelected = [...this.state.selected]
+
+      if(newSelected.includes(element)) {
+        newSelected = newSelected.filter( each =>{ return each !== element })
+      } else {
+        newSelected.push(element)
+      }
+
+      let newPlaceholder = newSelected.length > this.maxElementPlaceholder? `${newSelected.length} ${this.labels.plural} ${this.labels.select}`: newSelected.map(each => each.label).join(", ")
+
+      this.setState({selected: newSelected, placeholder: newPlaceholder})
+
+    } else {
+      this.setState({selected: [element], placeholder: element.label})
+    }
+  };
+
+  selectAllElements = () => {
+    let newPlaceholder = this.data.length > this.maxElementPlaceholder? `${this.data.length} ${this.labels.plural} ${this.labels.select}`: this.data.map(each => each.label).join(", ")
+    this.setState({selected: [...this.data], placeholder: newPlaceholder})
+  }
+
+  deselectAllElements = () => {
+    this.setState({selected: [], placeholder: this.placeholderDefault})
+  }
+
+  filterData = event => {
+    console.log(event.target.value)
+    this.setState(
+      {
+        dataFiltered : [...this.data].filter(
+        (each) => each.label.toLowerCase().startsWith(event.target.value.toLowerCase()))
+      }
+    )
+  };
+
+
+
+  render = () => {
+    return (
+      <React.Fragment>
+        <div className="input-box">
+          <button
+            id="restboot-menu-button"
+            onClick={this.closeOrOpen}
+            type="button"
+            className="btn btn-default dropdown-toggle show-special-title button-dropdown"
+          >
+            <span className="pull-left filter-option"></span>
+            <span className="pull-left special-title">
+              {this.state.placeholder
+                ? this.state.placeholder
+                : this.placeholderDefault}
+            </span>
+            &nbsp;
+            <span className="caret"></span>
+          </button>
+          <div className={"dropdown-menu " + (this.state.isOpen ? "open" : "")}>
+            <div className="bs-searchbox">
+              <input
+                type="text"
+                className="form-control"
+                onChange={this.filterData}
+              />
+            </div>
+
+            <div className={"bs-actionsbox " + (this.isMultiSelect && this.showButtonsSelectAll ? "": "hide")}>
+              <div className="btn-group btn-block">
+                <button id="restboot-selectall-button" onClick={this.selectAllElements}
+                  type="button"
+                  className="actions-btn bs-select-all btn btn-default select-all-button"
+                >
+                  {this.labels.selectAll}
+                </button>
+                <button id="restboot-deselectall-button" onClick={this.deselectAllElements}
+                  type="button"
+                  className="actions-btn bs-deselect-all btn btn-default deselect-all-button"
+                >
+                  {this.labels.deselectAll}
+                </button>
+              </div>
+            </div>
+
+            
+            <ul id="restboot-dropdown-list" className="dropdown-menu inner">
+              {this.state.dataFiltered.map(each => {
+                return (
+                  <li key={each.label + each.index}>
+                    <a
+                      onClick={() => {
+                        if(!this.isMultiSelect) this.closeOrOpen();
+                        this.selectElement(each);
+                      }}
+                    >
+                      {each.label}
+                      <span
+                        className={
+                          this.state.selected.indexOf(each) >= 0
+                            ? "glyphicon glyphicon-ok"
+                            : ""
+                        }
+                      ></span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+            
+
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  };
+}
+  
+export default Dropdown;
---
+++ src/__tests__/Dropdown-test.js
@@ -0,0 +1,176 @@
+import React from 'react';
+import {cleanup, fireEvent, render, create} from '@testing-library/react';
+import Dropdown from '../index';
+import Enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({adapter: new Adapter()})
+
+// automatically unmount and cleanup DOM after the test is finished.
+afterEach(cleanup)
+
+describe('Dropdown tests', () => {
+
+  it('Click select/deselect all', () => {
+    
+
+    const component = shallow(
+      <Dropdown
+      isMultiSelect={true}
+      showButtonsSelectAll={true}
+      labelElement="serviceId"
+      data={["AA", "AB", "BB", "CC", "DD", "BB", "EE", "FF", "GG"]}
+      maxElementPlaceholder="5"
+      labels={{deselectAll: "NoneSelected",selectAll: "AllSelected"}}
+    ></Dropdown>,
+    );
+
+    expect(component.find("#restboot-dropdown-list").html()).toEqual("<ul id=\"restboot-dropdown-list\" class=\"dropdown-menu inner\"><li><a>AA<span class=\"\"></span></a></li><li><a>AB<span class=\"\"></span></a></li><li><a>BB<span class=\"\"></span></a></li><li><a>CC<span class=\"\"></span></a></li><li><a>DD<span class=\"\"></span></a></li><li><a>BB<span class=\"\"></span></a></li><li><a>EE<span class=\"\"></span></a></li><li><a>FF<span class=\"\"></span></a></li><li><a>GG<span class=\"\"></span></a></li></ul>")
+    
+    component.find('#restboot-selectall-button').simulate('click');
+    expect(component.find("#restboot-dropdown-list").html()).toEqual("<ul id=\"restboot-dropdown-list\" class=\"dropdown-menu inner\"><li><a>AA<span class=\"glyphicon glyphicon-ok\"></span></a></li><li><a>AB<span class=\"glyphicon glyphicon-ok\"></span></a></li><li><a>BB<span class=\"glyphicon glyphicon-ok\"></span></a></li><li><a>CC<span class=\"glyphicon glyphicon-ok\"></span></a></li><li><a>DD<span class=\"glyphicon glyphicon-ok\"></span></a></li><li><a>BB<span class=\"glyphicon glyphicon-ok\"></span></a></li><li><a>EE<span class=\"glyphicon glyphicon-ok\"></span></a></li><li><a>FF<span class=\"glyphicon glyphicon-ok\"></span></a></li><li><a>GG<span class=\"glyphicon glyphicon-ok\"></span></a></li></ul>")
+
+    component.find('#restboot-deselectall-button').simulate('click');
+    expect(component.find("#restboot-dropdown-list").html()).toEqual("<ul id=\"restboot-dropdown-list\" class=\"dropdown-menu inner\"><li><a>AA<span class=\"\"></span></a></li><li><a>AB<span class=\"\"></span></a></li><li><a>BB<span class=\"\"></span></a></li><li><a>CC<span class=\"\"></span></a></li><li><a>DD<span class=\"\"></span></a></li><li><a>BB<span class=\"\"></span></a></li><li><a>EE<span class=\"\"></span></a></li><li><a>FF<span class=\"\"></span></a></li><li><a>GG<span class=\"\"></span></a></li></ul>")
+
+  
+  });
+
+  it('Filter list', () => {
+    
+    const component = shallow(
+      <Dropdown
+      isMultiSelect={true}
+      showButtonsSelectAll={true}
+      labelElement="serviceId"
+      data={["AA", "AB", "BB", "CC", "DD", "BB", "EE", "FF", "GG"]}
+      maxElementPlaceholder="5"
+      labels={{deselectAll: "NoneSelected",selectAll: "AllSelected"}}
+    ></Dropdown>,
+    );
+
+    expect(component.find("#restboot-dropdown-list").html()).toEqual("<ul id=\"restboot-dropdown-list\" class=\"dropdown-menu inner\"><li><a>AA<span class=\"\"></span></a></li><li><a>AB<span class=\"\"></span></a></li><li><a>BB<span class=\"\"></span></a></li><li><a>CC<span class=\"\"></span></a></li><li><a>DD<span class=\"\"></span></a></li><li><a>BB<span class=\"\"></span></a></li><li><a>EE<span class=\"\"></span></a></li><li><a>FF<span class=\"\"></span></a></li><li><a>GG<span class=\"\"></span></a></li></ul>")
+    
+    let eventObj = { target: { value: 'AA' } };
+    component.find('input').simulate('change', eventObj);
+    expect(component.find("#restboot-dropdown-list").html()).toEqual("<ul id=\"restboot-dropdown-list\" class=\"dropdown-menu inner\"><li><a>AA<span class=\"\"></span></a></li></ul>")
+
+    eventObj = { target: { value: 'A' } };
+    component.find('input').simulate('change', eventObj);
+    expect(component.find("#restboot-dropdown-list").html()).toEqual("<ul id=\"restboot-dropdown-list\" class=\"dropdown-menu inner\"><li><a>AA<span class=\"\"></span></a></li><li><a>AB<span class=\"\"></span></a></li></ul>")
+  });
+  
+  it('Singular/plural labels', () => {
+
+    const component = shallow(
+      <Dropdown
+      isMultiSelect={true}
+      showButtonsSelectAll={true}
+      labelElement="serviceId"
+      data={["AA", "AB", "BB", "CC", "DD", "BB", "EE", "FF", "GG"]}
+      maxElementPlaceholder="5"
+      labels={{singular: "car", plural: "cars"}}
+    ></Dropdown>,
+    );
+
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">Select car</span> <span class=\"caret\"></span></button>")
+    
+    component.find('#restboot-selectall-button').simulate('click');
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">9 cars selected</span> <span class=\"caret\"></span></button>")
+
+    component.find('#restboot-deselectall-button').simulate('click');
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">Select car</span> <span class=\"caret\"></span></button>")
+
+  
+  });
+
+  it('Select first element', () => {
+
+    const component = shallow(
+      <Dropdown
+      isMultiSelect={true}
+      showButtonsSelectAll={true}
+      labelElement="serviceId"
+      data={["AA", "AB", "BB", "CC", "DD", "BB", "EE", "FF", "GG"]}
+      maxElementPlaceholder="5"
+      labels={{singular: "car", plural: "cars"}}
+    ></Dropdown>,
+    );
+
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">Select car</span> <span class=\"caret\"></span></button>")
+    shallow(component.find("a").get(0)).simulate("click")
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">AA</span> <span class=\"caret\"></span></button>")  
+    
+  });
+
+  it('Select three elements, maxElementPlaceholder = 2', () => {
+
+    const component = shallow(
+      <Dropdown
+      isMultiSelect={true}
+      showButtonsSelectAll={true}
+      labelElement="serviceId"
+      data={["AA", "AB", "BB", "CC", "DD", "BB", "EE", "FF", "GG"]}
+      maxElementPlaceholder="2"
+      labels={{singular: "car"}}
+    ></Dropdown>,
+    );
+
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">Select car</span> <span class=\"caret\"></span></button>")
+    shallow(component.find("a").get(0)).simulate("click")
+    shallow(component.find("a").get(1)).simulate("click")
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">AA, AB</span> <span class=\"caret\"></span></button>")  
+    shallow(component.find("a").get(2)).simulate("click")
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">3 items selected</span> <span class=\"caret\"></span></button>")  
+
+  });
+
+  it('Single select', () => {
+
+    const component = shallow(
+      <Dropdown
+      isMultiSelect={false}
+      labelElement="serviceId"
+      data={["AA", "AB", "BB", "CC", "DD", "BB", "EE", "FF", "GG"]}
+      maxElementPlaceholder="2"
+      labels={{singular: "car"}}
+    ></Dropdown>,
+    );
+
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">Select car</span> <span class=\"caret\"></span></button>")
+    shallow(component.find("a").get(0)).simulate("click")
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">AA</span> <span class=\"caret\"></span></button>")  
+    shallow(component.find("a").get(1)).simulate("click")
+    expect(component.find("#restboot-menu-button").html()).toEqual("<button id=\"restboot-menu-button\" type=\"button\" class=\"btn btn-default dropdown-toggle show-special-title button-dropdown\"><span class=\"pull-left filter-option\"></span><span class=\"pull-left special-title\">AB</span> <span class=\"caret\"></span></button>")  
+
+  });
+  
+
+  it('Select/deselect all labels', () => {
+    
+
+    const {getByText} = render(
+      <Dropdown
+      isMultiSelect={true}
+      showButtonsSelectAll={true}
+      labelElement="serviceId"
+      data={["AA", "AB", "BB", "CC", "DD", "BB", "EE", "FF", "GG"]}
+      maxElementPlaceholder="5"
+      labels={{deselectAll: "NoneSelected",selectAll: "AllSelected"}}
+    ></Dropdown>,
+    );
+    
+    expect(getByText(/AllSelected/i)).toBeTruthy()
+    expect(getByText(/NoneSelected/i)).toBeTruthy()
+
+    //expect(Object.is(getByText(/NoneSelected/i),getByText(/NoneSelected/i))).toBe(true) 
+    //expect(getByText(/NoneSelected/i)).toBe("ciao")
+    //expect(getByText(/NoneSelected/i)).toBe('<button class="actions-btn bs-deselect-all btn btn-default deselect-all-button" type="button">NoneSelected</button>')
+   
+  
+    //fireEvent.click(getByLabelText(/selettato/i));
+  
+    //expect(queryByLabelText(/on/i)).toBeTruthy();
+  });
+  
+});

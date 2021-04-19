dd302382d7ae2a784a58daf68732fee4e6de9bdd
bolognesimarco <bolognesi.marco@gmail.com>
2021-04-19 11:22:27 +0200

    Update rdoc.py

--- .auto/rdoc.py
+++ .auto/rdoc.py
@@ -1,19 +1,19 @@
 import os
 import re
 import sys
-import base64
 
 def main(version):
-    if len(sys.argv) != 2:
-        print("[ERROR] Missing argument version (PREVIOUS) such as: X.Y.Z")
+    if len(sys.argv) != 4:
+        print("[ERROR] Missing one of arguments: version (PREVIOUS) such as: X.Y.Z or userid or token")
         sys.exit()
     version = sys.argv[1]
+    userid = sys.argv[2]
+    token = sys.argv[3]
     ver_pattern = re.compile("^[0-9]+\.[0-9]+\.[0-9]+$")
     if not ver_pattern.match(version):
         print("[ERROR] Wrong argument version (PREVIOUS). It must be: X.Y.Z")
         sys.exit()
-    decodedtok = str(base64.b64decode('Z2hwX0VtdEtCankzcW9UejhkRDhBUkJQRkpyd1J4UGgxTTJWR3Vlcg=='))
-    cmd = 'github_changelog_generator --user fdefelici --project react-bootstrap-combobox -t {decodedtok} --since-tag v{version} --no-unreleased'.format(**locals())
+    cmd = 'github_changelog_generator --user {userid} --project react-bootstrap-combobox -t {token} --since-tag v{version} --no-unreleased'.format(**locals())
     os.system(cmd)
 
 if __name__ == "__main__":
